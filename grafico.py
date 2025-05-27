18397ad3ec2400e79b2bc7e52385bcad011817c2
raquelsantos81 <raquelzinhasantosfaria@gmail.com>
2025-05-27 15:37:48 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,8 +1,13 @@
 # https://python-graph-gallery.com/
-#
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
+try: 
+  tot_registros = df.count()["total"]
+except:
+
 df = pd.read_csv('https://perfil-i.ibict.br/media/uploads/user_sum.csv')
-df
+
+st.bar_chart(df,x='month', y='total')
 
